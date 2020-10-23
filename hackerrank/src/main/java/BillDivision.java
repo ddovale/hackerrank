7c5d09eea3e298a79bdc5e7f72d845a30b832e42
Daniel do Vale <daniel.vale@intelipost.com.br>
2020-10-23 14:28:58 -0300

    Bill Division

---
+++ hackerrank/src/main/java/BillDivision.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class BillDivision {
+
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<Integer> bill, int k, int b) {
+
+        Integer billSum = bill.stream()
+                .reduce(0, Integer::sum);
+
+        int charged = (billSum - bill.get(k)) / 2;
+
+        System.out.println( b > charged ? b - charged : "Bon Appetit");
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nk = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
